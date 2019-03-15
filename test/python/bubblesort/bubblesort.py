87a15e599f6785d1f0caddc93901d9b4a69e525d
root <davidenannif@gmail.com>
2019-03-15 18:31:34 +0100

    Add Python Results Bubblesort Notebook

--- test/python/bubblesort/bubblesort.py
+++ test/python/bubblesort/bubblesort.py
@@ -30,6 +30,21 @@ def bubblesort_test(workload):
 	t2 = datetime.datetime.now()
 	t3 = t2 - t1
 	elapsed_ms = (t3.seconds * 1000) + (t3.microseconds / 1000)
-	print(elapsed_ms)
+	return elapsed_ms
 
-bubblesort_test(int(sys.argv[1]))	
+
+
+workload = int(sys.argv[1])
+numTest = int(sys.argv[2])
+testFile = sys.argv[3]
+
+file = open(testFile,'w') 
+
+
+for i in range(0, numTest):
+	elapsed_ms = bubblesort_test(workload)
+	elapsed_ms_str = str(elapsed_ms)
+	file.write(elapsed_ms_str)
+	file.write('\n')
+
+file.close()
